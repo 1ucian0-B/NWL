91b473236223e56a51cefe1ed0e49d15782807c5
LucianoFernandesBrido <bridoluciano@gmail.com>
2020-10-18 09:07:06 -0400

    integração com maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
 import React from 'react';
 import './Styles/global.css';
-import logoImg from './Images/Logo.svg';
-import './Styles/Pages/landing.css';
+import Routes from './routes';
 
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt=" Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Vesite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>Rio do Sul</strong>
-          <span>Guarda</span>
-        </div>
-
-        <a href="" className="enter-app">
-
-          >
-        </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
